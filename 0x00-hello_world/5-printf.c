2fce6e9e5774748453959b2286aba2d8f18bcf84
dorrisjeruto77 <dorrisjeruto@gmail.com>
2023-04-20 08:00:03 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,6 +5,6 @@
 */
 int main(void)
 {
-        printf("proper grammar, but the outcome is a piece of art,\n");
-       return (0);
+printf("proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }      
