51c5db17ba926781cf6f5adfcd1d1aa48aa8c5bd
dorrisjeruto77 <dorrisjeruto@gmail.com>
2023-04-20 06:28:39 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+/**
+ * main - This program uses puts function to print a line
+ * Return: 0
+ */
+int main (void) 
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
