ed71d6001765a844c0fa7e936e10dcc29e8c121f
dorrisjeruto77 <dorrisjeruto@gmail.com>
2023-04-24 07:12:17 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -25,7 +25,7 @@ int main(void)
 		printf("last digit of %d is %d and is 0\n", e, n);
 	else if
 	{
-		printf("last digit of %d is %d and is less than 6 and not 0\n", e, n);
+		printf("last digit of %d is %d and is less than 6 and not 0\, e, n);
 	}
 	return (0);
 }
