d1cc7f15f308669255bb4d68bb4cabe0d1e1964a
dorrisjeruto77 <dorrisjeruto@gmail.com>
2023-04-24 03:12:44 -0700

    5-print_numbers.c

---
+++ 0x00-hello_world/0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers of base 10 beggining from 0.
+ * Return:0 (success)
+ */
+
+int main(void)
+
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+	printf("\n");
+return (0);
+}
