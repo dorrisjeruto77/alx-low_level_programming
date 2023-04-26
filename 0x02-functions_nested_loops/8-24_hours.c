fbc674a639bfa037fe706997d052644817cac3a1
dorrisjeruto77 <dorrisjeruto@gmail.com>
2023-04-26 14:10:49 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * jack_bauer - Check main
+ * Description: function prints every minute in one day
+ * Return: Nothing
+ */
+
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0; hour <= 23; hour++)
+	{
+		for (minute = 0; minute <= 59; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
