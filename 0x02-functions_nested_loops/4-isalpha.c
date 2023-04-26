ec93f1a5d72ac9754f0ce9ad1717da2826793582
dorrisjeruto77 <dorrisjeruto@gmail.com>
2023-04-26 13:12:30 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _isalpha - check main
+ * @c: An input character
+ * Description : check for alphabetic character.
+ * Return: 1 if c is a letter, lowercase or uppercase and 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
