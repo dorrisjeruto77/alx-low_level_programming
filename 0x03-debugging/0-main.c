c1fee485c27e72a8c0e84560761424dadb4468dd
dorrisjeruto77 <aldo@localhost.localdomain>
2023-04-27 07:54:17 -0400

    Debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,11 +3,12 @@
  * main - Test function for positive or negavite
  * Return: 0
  * /
- 
+
  int main(void)
  {
     int i;
+    
     i=0;
     positive_or_negative(i);
-return (0);
+    return (0);
 }
