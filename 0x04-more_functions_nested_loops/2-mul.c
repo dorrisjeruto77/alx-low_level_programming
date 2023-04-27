eac4d6abb91518db7c5205fd90b9d5f10b76e842
dorrisjeruto77 <aldo@localhost.localdomain>
2023-04-27 15:53:33 -0400

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - multiplies two integers.
+ * @b: input integer
+ * @a: input integer
+ * Return: 1 if c is uppercase and 0 otherwise.
+ */
+int mul(int a, int b)
+{
+    int value;
+
+    value = a * b;
+
+     return (value);
+}
