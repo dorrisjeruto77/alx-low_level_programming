2e58e4186c642774f6878b8da8e301afbb707ea2
dorrisjeruto77 <aldo@localhost.localdomain>
2023-04-27 16:05:31 -0400

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - checks for uppercase character.
+ *
+ */
+
+void print_most_numbers(void)
+{
+    int i;
+
+        for (i = 0; i <= 9; i++)
+        {
+        if (i != 2 && i != 4)
+            _putchar(i + '0');
+          }
+
+          _putchar('\n');
+}
