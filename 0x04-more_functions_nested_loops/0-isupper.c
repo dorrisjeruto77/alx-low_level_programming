81b82541433e8bc2d769a6210933d746293d0fbc
dorrisjeruto77 <aldo@localhost.localdomain>
2023-04-28 16:19:38 -0400

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -14,12 +14,5 @@ int _isupper(int c)
       {
             return (1);
       }
-      else if (c <= 'Z')
-      {
-            return (0);
-      }
-      else
-      {       
-            return (0);
-      }
+      return (0);
 }
