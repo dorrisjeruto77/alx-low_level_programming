5df2ca67a77b35011ecc98bfb8a0dcc9acaf0e42
dorrisjeruto77 <aldo@localhost.localdomain>
2023-04-28 15:15:57 -0400

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,12 +6,14 @@
  * @c: input character
  * Return: 1 if c is uppercase and 0 otherwise.
  */
-
 int _isupper(int c)
 {
-       if (c >= 'A' && c <= 'Z')i
-            return (1);
-
-       else
-            return (0);
+       if (c >= 65 && c <= 90)
+       {     
+              return (1);
+       } 
+        else
+        {
+              return (0);
+        }
 }
