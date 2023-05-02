01dc2fd5e6ff7f9555eb7faf49a63c98fa9d620d
dorrisjeruto77 <aldo@localhost.localdomain>
2023-05-02 18:57:24 -0400

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -3,47 +3,20 @@
 #include <time.h>
 
 /**
- * main - Generates random valid passwords for the
- *        program 101-crackme.
- * Return: Always 0.
+ * main - generates random passwords for 101-crackme
+ * Return: zero
  */
-
 int main(void)
 {
-    char password[84];
-    int index = 0, sum = 0, diff_half1, diff_half2;
-
-    srand(time(0));
-
-    while (sum < 2772)
+    int sum;
+    char c;
+    srand(time(NULL));
+    while (sum <= 2645)
     {
-          password[index] = 33 + rand() % 94;
-          sum += password[index++];
+          c = rand() % 128;
+          sum += c;
+          putchar(c);
     }
-    password[index] = '\0';
-    if (sum != 2772)
-    {
-          diff_half1 = (sum - 2772) / 2;
-          diff_half2 = (sum - 2772) / 2;
-          if ((sum - 2772) % 2 != 0)
-                  diff_half1++;
-          for (index = 0; password[index]; index++)
-          {
-                  if (password[index] >= (33 + diff_half1))
-                  {
-                  password[index] -= diff_half1;
-                  break;
-          }   
-      }   
-          for (index = 0; password[index]; index++)
-          {
-              if (password[index] >= (33 + diff_half2))
-              {   
-                      password[index] -= diff_half2;
-                      break;
-              } 
-          }
-      }
-      printf("%s", password);
-      return (0);
+    putchar(2772 - sum);
+    return (0);
 }
