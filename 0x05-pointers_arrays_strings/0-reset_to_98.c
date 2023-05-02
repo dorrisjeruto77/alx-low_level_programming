62a0a5afedb7f5c075b19d922253ff5701b2c680
dorrisjeruto77 <aldo@localhost.localdomain>
2023-05-02 11:25:37 -0400

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -11,8 +11,8 @@ void reset_to_98(int *n);
     int n;
 
     n = 402;
-    printf("n=%d\n", n);
+   _putchar("n=%d\n", n);
     reset_to_98(&n);
-    printf("n=%d\n", n);
+    _putchar("n=%d\n", n);
     return (0);
 }
