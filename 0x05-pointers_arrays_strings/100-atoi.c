70b83a6b34950079149373e31290ed2f2b3a8e53
dorrisjeruto77 <aldo@localhost.localdomain>
2023-05-02 17:49:51 -0400

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -26,11 +26,11 @@ int _atoi(char *s)
       ni *= min;
       return (ni);
 }
-else if (*s >= '0' && *s <= '9')
-num = (num * 10) + (*s - '0');
-else if (num > 0)
-break;
-} 
-while (*s++);
-return (num * sign);
-}
+      else if (*s >= '0' && *s <= '9')
+      num = (num * 10) + (*s - '0');
+      else if (num > 0)
+      break;
+      } 
+      while (*s++);
+      return (num * sign);
+      }
