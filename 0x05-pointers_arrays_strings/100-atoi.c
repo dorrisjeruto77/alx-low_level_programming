bf501c41830d20f0ee551a331784a13ebeed9d15
dorrisjeruto77 <aldo@localhost.localdomain>
2023-05-03 14:19:31 -0400

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,53 +1,32 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
+#include "main.h"
 /**
- * main - Generates random valid passwords for the
- *        program 101-crackme.
- * Return: Always 0.
+ * _atoi - converts a string to an integer.
+ * @s: the string to convert
+ * Return: the converted string.
  */
-
-int main(void)
+int _atoi(char *s)
 {
-    char password[84];
-    int index = 0, sum = 0, diff_half1, diff_half2;
+        short boolean;
+        int i, minus, result;
 
-    srand(time(0));
+        i = minus = result = boolean = 0;
+        minus = -1;
 
-    while (sum < 2772)
-    {
-          password[index] = 33 + rand() % 94;
-      sum += password[index++];
-  }
+        while (s[i] != '\0')
+        {
+                if (s[i] == '-')
+                      minus *= -1;
 
-      password[index] = '\0';
-
-      if (sum != 2772)
-      {
-          diff_half1 = (sum - 2772) / 2;
-          diff_half2 = (sum - 2772) / 2;
-      if ((sum - 2772) % 2 != 0)
-      diff_half1++;
-
-      for (index = 0; password[index]; index++)
-      {
-      if (password[index] >= (33 + diff_half1))
-      {
-          password[index] -= diff_half1;
-          break;
-      }
-  }
-      for (index = 0; password[index]; index++)
-      {
-      if (password[index] >= (33 + diff_half2))
-      {
-          password[index] -= diff_half2;
-          break;
-      }
-  }
-}
-
-        printf("%s", password);
-        return (0);
+                if (s[i] >= '0' && s[i] <= '9')
+                {   
+                      result *= 10;
+                      result -= (s[i] - '0');
+                      boolean = 1;
+                }
+                else if (boolean == 1)
+                        break;
+                i++;
+          }
+          result *= minus;
+          return (result);
 }
