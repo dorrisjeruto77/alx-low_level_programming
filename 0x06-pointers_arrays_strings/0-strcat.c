d4d14b2224eff963eb038fb17dd26154891cebc0
dorrisjeruto77 <aldo@localhost.localdomain>
2023-05-04 15:18:38 -0400

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,16 +9,13 @@
 
 char *_strcat(char *dest, char *src)
 {
-int a = -1, i;
-for (i = 0; dest[i] != '\0'; i++)
-;
-do {
+    int a = -1, i;
+    for (i = 0; dest[i] != '\0'; i++);
+    do {
       a++;
       dest[i] = src[a];
       i++;
-} while (src[a] != '\0');
-
-return (dest);
+      } 
+    while (src[a] != '\0');
+    return (dest);
 }
-
-
