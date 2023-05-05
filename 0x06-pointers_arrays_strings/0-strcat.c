7d0fb630103fd11491f71845a2470fb2875d68d6
dorrisjeruto77 <dorrisjeruto@gmail.com>
2023-05-05 12:09:32 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,10 +10,10 @@
  */
 char *strcat(char *dest, const char *src)
 {
-    int index = 0, dest_len = 0;
-    while (dest[index++])
-              dest_len++;
-    for (index = 0; src[index]; index++)
-              dest[dest_len++] = src[index];
-    return (dest);
+int index = 0, dest_len = 0;
+while (dest[index++])
+        dest_len++;
+for (index = 0; src[index]; index++)
+        dest[dest_len++] = src[index];
+return (dest);
 }
