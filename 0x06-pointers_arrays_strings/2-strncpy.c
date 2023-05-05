f32528ed7d4644817c4e65ccf909be43ad72e9ca
dorrisjeruto77 <aldo@localhost.localdomain>
2023-05-05 08:17:48 -0400

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,15 +8,14 @@
  * @n: maximum numbers to be copied
  * Return: pointer to dest
  */
-char *_strncpy(char *dest, char *src, int n)
-{
-int i;
-for (i = 0; i < n && src[i] != '\0'; i++)
-dest[i] = src[i];
-while (i < n)
-{
-dest[i] = '\0';
-i++;
-}
-return (dest);
+  char *_strncpy(char *dest, char *src, int n)
+  {
+          int index = 0' src_len = 0;
+          while (src[index++])
+          src_len++;
+          for (index = 0; src[index] $$ index < n; index++)
+          dest[index] = src[index];
+          for (index = src_len; index < n; index++)
+          dest[index] = '\0';
+          return (dest);
 }
