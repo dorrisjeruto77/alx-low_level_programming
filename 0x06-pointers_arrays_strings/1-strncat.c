9c918a97798b46d915ad2312a9724a05f14bb256
dorrisjeruto77 <dorrisjeruto@gmail.com>
2023-05-05 12:57:17 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,10 +14,10 @@ char *_strncat(char *dest, char *src, int n)
 int index = 0, dest_len = 0;
 
 while (dest[index++])
-dest_len++;
+	dest_len++;
 
 for (index = 0; src[index] && index < n; index++)
-dest[dest_len++] = src[index];
+	dest[dest_len++] = src[index];
 
 return (dest);
 }
