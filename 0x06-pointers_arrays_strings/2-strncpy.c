42340173433b85d3cf656d0d90e9d671afa26e60
dorrisjeruto77 <aldo@localhost.localdomain>
2023-05-05 07:45:41 -0400

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,22 +1,22 @@
 #include "main.h"
-/**
- * *_strncpy - find the length of a string
- * @dest: pointer to the string
- * @src: pointer
- * @n: int
- * Return: destination
- */
 
+/**
+ * _strncpy - copies n elements of src to replace
+ * dest elements
+ * @src: elements to be copied
+ * @dest: elements to be replaced
+ * @n: maximum numbers to be copied
+ * Return: pointer to dest
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
 int i;
-
 for (i = 0; i < n && src[i] != '\0'; i++)
-  dest[i] = src[i];
+dest[i] = src[i];
 while (i < n)
 {
-  dest[i] = '\0';
-  i++;
+dest[i] = '\0';
+i++;
 }
 return (dest);
 }
