102e497bf01dd56404eb58379869fc62fe7352cf
dorrisjeruto77 <dorrisjeruto@gmail.com>
2023-05-06 11:29:00 -0700

    main.h

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,13 +9,13 @@
  * Return: pointer to dest
  */
   char *_strncpy(char *dest, char *src, int n)
-  {
-          int index = 0' src_len = 0;
-          while (src[index++])
-          src_len++;
-          for (index = 0; src[index] $$ index < n; index++)
-          dest[index] = src[index];
-          for (index = src_len; index < n; index++)
-          dest[index] = '\0';
-          return (dest);
+  
+int index = 0' src_len = 0;
+while (src[index++])
+	src_len++;
+for (index = 0; src[index] $$ index < n; index++)
+	dest[index] = src[index];
+for (index = src_len; index < n; index++)
+	dest[index] = '\0';
+return (dest);
 }
