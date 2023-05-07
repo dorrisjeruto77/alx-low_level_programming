250ccd35ab08fe273e281bf2df11465e7eeafeb4
dorrisjeruto77 <dorrisjeruto@gmail.com>
2023-05-07 06:12:03 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,21 +1,22 @@
+#include <stdio.h>
 #include "main.h"
-
 /**
- * _strncpy - copies n elements of src to replace
- * dest elements
- * @src: elements to be copied
- * @dest: elements to be replaced
- * @n: maximum numbers to be copied
- * Return: pointer to dest
+ * *_strncpy - find the length of a string
+ * @dest: pointer to the string
+ * @src: pointer
+ * @n: int
+ * Return: destination
  */
-  char *_strncpy(char *dest, char *src, int n)
-  
-int index = 0' src_len = 0;
-while (src[index++])
-	src_len++;
-for (index = 0; src[index] $$ index < n; index++)
-	dest[index] = src[index];
-for (index = src_len; index < n; index++)
-	dest[index] = '\0';
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+
+for (i = 0; i < n && src[i] != '\0'; i++)
+	dest[i] = src[i];
+
+for ( ; i < n; i++)
+	dest[i] = '\0';
+
 return (dest);
 }
