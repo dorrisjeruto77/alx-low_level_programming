08649d63360b397fa53d65abca11d94d4cac1cb8
dorrisjeruto77 <dorrisjeruto@gmail.com>
2023-05-08 08:53:59 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 
 /**
- *  * _strstr -  a function that locates a substring.
- *   * @haystack: an input string to search in
- *    * @needle: an input string to locate into string haystack
- *     * Return:  a pointer to the beginning of the located substring,
- *      * or NULL if the substring is not found.
- *       */
+ *_strstr -  a function that locates a substring.
+ *@haystack: an input string to search in
+ *@needle: an input string to locate into string haystack
+ *Return:  a pointer to the beginning of the located substring,
+ *or NULL if the substring is not found.
+ */
 
 char *_strstr(char *haystack, char *needle)
 {
