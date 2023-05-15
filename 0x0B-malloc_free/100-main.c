d54df4de0e8b566fb59408f6b28b2e380396804d
dorrisjeruto77 <dorrisjeruto@gmail.com>
2023-05-15 13:51:47 -0700

    101-strtow.c

--- 0x0B-malloc_free/100-main.c
+++ 0x0B-malloc_free/100-main.c
@@ -3,19 +3,34 @@
 #include <stdlib.h>
 
 /**
- * main - check the code for ALX School students.
- * Return: Always 0.
+ * print_tab - Prints an array of string
+ * @tab: The array to print
+ * Return: nothing
  */
-int main(int ac, char *av[])
+void print_tab(char **tab)
 {
-	char *s;
+	int i;
 
-	s = argstostr(ac, av);
-	if (s == NULL)
+	for (i = 0; tab[i] != NULL; ++i)
 	{
+		printf("%s\n", tab[i]);
+	}
+}
+
+/**
+ * main - check the code for ALX School students.
+ * Return: 1 if an error occurred, 0 otherwise
+ */
+int main(void)
+{
+	char **tab;
+
+	tab = strtow("      ALX School         #cisfun      ");
+	if (tab == NULL)
+	{
+		printf("Failed\n");
 		return (1);
 	}
-	printf("%s", s);
-	free(s);
+	print_tab(tab);
 	return (0);
 }
