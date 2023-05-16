d7fb3270b26df78f0094fe74f1a95d6e0aa8fa95
dorrisjeruto77 <dorrisjeruto@gmail.com>
2023-05-16 11:38:55 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,6 +10,14 @@
 
 int main(int argc, char *argv[])
 {
-	printf("%d\n", argc - 1);
+	int i;
+
+	if (argc == 1)
+		printf("%d\n", argc - 1);
+	else
+	{
+		for (i = 0; *argv; i++, argv++)
+		printf("%d\n" i - 1);
+	}
 	return (0);
 }
