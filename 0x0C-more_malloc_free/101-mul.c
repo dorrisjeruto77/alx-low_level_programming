3602f9d447d8c79c381f36147e1f428226ca9f51
dorrisjeruto77 <dorrisjeruto@gmail.com>
2023-05-18 15:35:21 -0700

    100-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -15,9 +15,10 @@ int is_digit(char *s)
 
 	while (s[i])
 	{
-	if (s[i] < '0' || s[i] > '9')
-		return (0)						
-	i++;
+
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
 	}
 	return (1);
 }
@@ -30,6 +31,7 @@ int is_digit(char *s)
 int _strlen(char *s)
 
 	int i = 0;
+
 	while (s[i] != '\0')
 	{
 		i++;
@@ -56,6 +58,7 @@ int main(int argc, char *argv[])
 {
 	char *s1, *s2;
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
@@ -71,13 +74,14 @@ int main(int argc, char *argv[])
 	{
 		digit1 = s1[len1] - '0';
 		carry = 0;
-	for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
-	{
-		digit2 = s2[len2] - '0';
-		carry += result[len1 + len2 + 1] + (digit1 * digit2);
-		result[len1 + len2 + 1] = carry % 10;
-		carry /= 10;
-	}
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		{
+
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
 		if (carry > 0)
 			result[len1 + len2 + 1] += carry;
 	}
