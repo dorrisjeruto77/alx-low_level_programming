d5d653b69f7ee573b8461e2fe4a8c17978ae2cd1
dorrisjeruto77 <dorrisjeruto@gmail.com>
2023-05-19 10:53:04 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,13 +8,12 @@
 
 void print_most_numbers(void)
 {
-    int i;
+int i;
 
-        for (i = 0; i <= 9; i++)
-        {
-        if (i != 2 && i != 4)
-            _putchar(i + '0');
-          }
-
-          _putchar('\n');
+for (i = 0; i <= 9; i++)
+{
+if (i != 2 && i != 4)
+_putchar(i + '0');
+}
+_putchar('\n');
 }
