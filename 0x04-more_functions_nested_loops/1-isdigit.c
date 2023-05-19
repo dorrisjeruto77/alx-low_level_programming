4bb04f8bfab5c2ee067fc6342c430433fa6d9736
dorrisjeruto77 <dorrisjeruto@gmail.com>
2023-05-19 10:34:52 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -10,9 +10,9 @@
 
 int _isdigit(int c)
 {
-      if (c >= '0' && c <= '9')
-          return (1);
+if (c >= '0' && c <= '9')
+return (1);
 
-      else
-            return (0);
+else
+return (0);
 }
