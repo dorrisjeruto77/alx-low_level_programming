9d49722ceafb8b5f3a903475ebcbb25f547ac0d8
dorrisjeruto77 <dorrisjeruto@gmail.com>
2023-05-19 10:47:14 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,12 +8,11 @@
 
 void print_numbers(void)
 {
-    int i;
+int i;
 
-    for (i = 0; i <= 9; i++)
-    {
-          _putchar(i + '0');
-    }
-
-    _putchar('\n');
+for (i = 0; i <= 9; i++)
+{
+_putchar(i + '0');
+}
+_putchar('\n');
 }
