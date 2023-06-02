83acf063dd8b17e9e1eca770b265dbbd2849c246
dorrisjeruto77 <dorrisjeruto77@gmail.com>
2023-06-02 15:59:34 -0700

    4-free_list.c

--- 0x12-singly_linked_lists/4-main.c
+++ 0x12-singly_linked_lists/4-main.c
@@ -15,11 +15,11 @@ int main(void)
 	head = NULL;
 	add_node_end(&head, "Bob");
 	add_node_end(&head, "&");
- 	add_node_end(&head, "Kris");
- 	add_node_end(&head, "love");
- 	add_node_end(&head, "asm");
- 	print_list(head);
- 	free_list(head);
+	add_node_end(&head, "Kris");
+	add_node_end(&head, "love");
+	add_node_end(&head, "asm");
+	print_list(head);
+	free_list(head);
 	head = NULL;
 	return (0);
 }
