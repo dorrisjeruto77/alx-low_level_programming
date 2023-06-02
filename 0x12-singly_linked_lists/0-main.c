b09f14a01f2332aa04cb3f2c9c976b6eb2da3d17
dorrisjeruto77 <dorrisjeruto77@gmail.com>
2023-06-02 15:08:06 -0700

    1-list_len.c

--- 0x12-singly_linked_lists/0-main.c
+++ 0x12-singly_linked_lists/0-main.c
@@ -19,8 +19,8 @@ int main(void)
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 	{
-	printf("Error\n");
-	return (1);
+		printf("Error\n");
+		return (1);
 	}
 	new->str = strdup("Hello");
 	new->len = 5;
--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,6 +3,7 @@
 /**
  * print_list - prints all the elements of a linked list
  * @h: pointer to the list_t list to print
+ * elements of all a list_t list
  * Return: the number of nodes printed
  */
 
@@ -18,7 +19,7 @@ size_t print_list(const list_t *h)
 
 		else
 
-		printf("[%u] %s\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 
 		h = h->next;
 		node++;
