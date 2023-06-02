dd4ddafe1ad26d05b69a5cf9052e63a03ccb5ae3
dorrisjeruto77 <dorrisjeruto77@gmail.com>
2023-06-02 11:50:56 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
 /**
@@ -9,20 +7,21 @@
  */
 
 size_t print_list(const list_t *h)
-	size_t count = 0;
+{
+	unsigned int nodes = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		if (h->str == NULL)
-		{
-			printf("[0] (nil)\n");
-		}
+
+			printf("[0](nil)\n");
+
 		else
-		{
-		printf("[%d] %s\n", h->len, h->str);
-		}
+
+		printf("[%u] %s\n", h->len, h->str);
+
 		h = h->next;
-		count++;
+		node++;
 	}
-	return (s);
+	return (nodes);
 }
