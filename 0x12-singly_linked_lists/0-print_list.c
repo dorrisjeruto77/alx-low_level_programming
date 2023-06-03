61420baf6776a256b7926621cc68f213502f7a81
dorrisjeruto77 <dorrisjeruto77@gmail.com>
2023-06-02 17:10:11 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,21 +10,20 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
+	size_t s = 0;
 
-	while (h != NULL)
+	while (h)
 	{
-		if (h->str == NULL)
+	if (!h->str)
 
-			printf("[0](nil)\n");
+	printf("[0](nil)\n");
 
-		else
+	else
 
-			printf("[%d] %s\n", h->len, h->str);
+	printf("[%u] %s\n", h->len, h->str);
 
-		node++;
-		h = h->next;
+	s++;
 	}
 
-	return (nodes);
+	return (s);
 }
