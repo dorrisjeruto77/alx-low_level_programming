d4690332d346ca4e893bbfbaa8643e7aa9277cce
dorrisjeruto77 <dorrisjeruto77@gmail.com>
2023-06-04 09:52:53 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,46 +1,41 @@
 #include "lists.h"
-
-/**
- *_strlen - get the lenght of a string
- * @s: string
- * Return: Always 0
- */
-int - strlen(const char *s)
-{
-	int lenght = 0;
-
-	while (*(s + length))
-		length++;
-	return (lenghth);
-}
+#include <string.h>
+#include <stdlib.h>
 
 /**
  * add_node - adds a new node at the beginning of a linked list
  * @head:double pointer to the list_t list
- * @st: pointer to string
+ * @str: string to be duplicated
+ * Pointer to the new node
  *
  * Return:the address of the new element, or NULL if it fails
  */
 
-list_t *add_node(list_t **head, const char *str)
-{
-	list_t *new_node;
+list_t *add_node(list_t **head, const char *str);
 
-	if (head == NULL)
-		return (NULL);
+	list_t *new_node;
+	char *dup_str;
+	int len = 0;
 
 	new_node = malloc(sizeof(list_t));
+
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
+	dup_str = strdup(str);
+
+	if (dup_str == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
-	new_node->len = _strlen(str);
+	while (str[len])
+		len++;
+
+	new_node->str = dup_str;
+	new_node->len = len;
 	new_node->next = *head;
+
 	*head = new_node;
 	return (new_node);
 }
