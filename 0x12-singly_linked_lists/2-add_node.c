33c96b9880903e7f1cff0b693194054c958a6093
dorrisjeruto77 <dorrisjeruto77@gmail.com>
2023-06-04 07:52:43 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -21,10 +21,11 @@ list_t *add_node(list_t **head, const char *str)
 	new = malloc(sizeof(list_t));
 	if (!new)
 		return (NULL);
+		
 	new->str = strdup(str);
 	new->len = len;
 	new->next = (*head);
 	(*head) = new;
 
-	return (*head);
+	return (new);
 }
