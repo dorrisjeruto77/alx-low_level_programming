408caa9e0853e46e6e0e73c46e0d562b5da32b63
dorrisjeruto77 <dorrisjeruto77@gmail.com>
2023-06-05 11:32:05 -0700

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-main.c
+++ 0x13-more_singly_linked_lists/7-main.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
@@ -9,7 +10,7 @@
 int main(void)
 {
 	listint_t *head;
-	int n;
+	listint_t *node;
 
 	head = NULL;
 	add_nodeint_end(&head, 0);
@@ -21,13 +22,9 @@ int main(void)
 	add_nodeint_end(&head, 402);
 	add_nodeint_end(&head, 1024);
 	print_listint(head);
-	n = pop_listint(&head);
-	printf("- %d\n", n);
-	print_listint(head);
-	n = pop_listint(&head);
-	printf("- %d\n", n);
+	node = get_nodeint_at_index(head, 5);
+	printf("%d\n", node->n);
 	print_listint(head);
 	free_listint2(&head);
-	printf("%p\n", (void *)head);
 	return (0);
 }
