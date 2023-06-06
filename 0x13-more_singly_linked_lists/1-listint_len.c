052db3eba7b5420f283f526397f0a6002d5380ca
dorrisjeruto77 <dorrisjeruto77@gmail.com>
2023-06-05 17:07:22 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -5,10 +5,9 @@
  * @h:linked list of type listint_t to traverse
  * Return: number of nodes
  */
- 
- size_t listint_len(const listint_t *h)
- {
- 	size_t num = 0;
+size_t listint_len(const listint_t *h)
+{
+size_t num = 0;
 
 	while (h)
 	{
