79f68a61646b12987714ce299a2a0dd2be4e109f
dorrisjeruto77 <dorrisjeruto77@gmail.com>
2023-06-05 18:17:05 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -34,7 +34,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		if (i == idx - 1)
 		{
-			new->next = current;
+			new->next = temp->next;
 			temp->next = new;
 			return (new);
 		}
