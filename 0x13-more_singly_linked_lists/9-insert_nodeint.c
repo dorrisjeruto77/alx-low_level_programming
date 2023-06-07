70bbdc872a7a86a07af82ad39b628b4b33fee8cd
dorrisjeruto77 <dorrisjeruto77@gmail.com>
2023-06-07 05:52:56 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,7 +28,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new);
 	}
 
-	for (i = 0; i < idx -1; i++)
+	for (i = 0; i < idx; i++)
 	{
 		if (i == idx - 1)
 		{
