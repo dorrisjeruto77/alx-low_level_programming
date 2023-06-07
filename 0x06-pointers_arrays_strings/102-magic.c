5c8c69adbd079b8f5add11bfddc36e879c43d7d7
dorrisjeruto77 <dorrisjeruto77@gmail.com>
2023-06-07 07:38:50 -0700

    correction

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -9,14 +9,13 @@ int main(void)
 	p = &n;
 /*
 * write your line of code here...
-* main - entry point
 * Remember:
 * - you are not allowed to use a
 * - you are not allowed to modify p
 * - only one statement
 * - you are not allowed to code anything else than this line of code
 */
-*(p + 7) = 98;
+*(p + 5) = 98;
 /* ...so that this prints 98\n */
 	printf("x[2] = %d\n", x[2])
 	return (0);
