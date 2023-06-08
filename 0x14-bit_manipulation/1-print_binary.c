0546607c5249a3f4f3359994071d898de7df7363
dorrisjeruto77 <dorrisjeruto77@gmail.com>
2023-06-08 13:03:28 -0700

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * @n: number to print in binary
+ * print_binary - prints the binary representation  number
+ *
+ */
+
+void print_binary(unsigned long int n);
+{
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
+}
