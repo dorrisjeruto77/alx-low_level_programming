019053f0537abbacc11df533b4da0fcf967d2281
dorrisjeruto77 <dorrisjeruto77@gmail.com>
2023-06-09 07:50:22 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,7 +8,7 @@
  * Return: value of the bit at index or -1 if an error occured
  */
 
-int get_bit(unsigned long int n, unsigned int index);
+int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int get;
 
