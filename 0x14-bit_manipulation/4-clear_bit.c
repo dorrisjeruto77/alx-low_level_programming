1e5a20dc884cb997d728c45bc4ed5ac1a5a35b34
dorrisjeruto77 <dorrisjeruto77@gmail.com>
2023-06-09 11:39:16 -0700

    4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -6,7 +6,7 @@
  * Return: returns 1 if it worked, or -1 if an error occured
  */
 
-int clear_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
