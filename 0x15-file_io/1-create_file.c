255e995942d2cbf8e92d1d21d4442effde03293c
dorrisjeruto77 <dorrisjeruto77@gmail.com>
2023-06-12 11:21:02 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,5 @@
-#include <string.h>
 #include "main.h"
+#include <string.h>
 
 /**
  *create_file - creates a file if does not exist and writes text to it
@@ -14,12 +14,12 @@ int create_file(const char *filename, char *text_content)
 	int len;
 
 	if (filename == NULL)
-		return (-1)
+		return (-1);
 
 	if (text_content == NULL)
 		text_content = " ";
 
-	len = strlen(text_context);
+	len = strlen(text_content);
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	fw = write(fd, text_content, len);
 
