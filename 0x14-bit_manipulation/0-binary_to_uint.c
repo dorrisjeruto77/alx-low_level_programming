abed9763e57bd7ad57e6d9c2cab56e590a35712d
dorrisjeruto77 <dorrisjeruto77@gmail.com>
2023-10-26 08:17:09 -0700

    0x14-bit_manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,11 +1,22 @@
 #include "main.h"
 /**
- * @n:The decimal number to be printed in binary
- * print_binary - prints the binary representation of a decimal number
+ * @b: binary
+ * binary_to_uint - converts a binary number to an unsigned interger
+ * return: unsigned int
  */
-void print_binary(unsigned long int n)
+unsigned int binary_to_uint(const char *b)
 {
-	if (n > 1)
-		print_binary(n >> 1);
-	_putchar((n & 1) + '0');
+	int i;
+
+	unsigned int dec_val = 0;
+
+	if (!b)
+		return (0);
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		dec_val = 2 * dec_val + (b[i] - '0');
+	}
+	return (dec_val);
 }
